0401e5046e8233e2cd140499ed00dae914168744
Dhaval Mehta <dhavalmehta@logisticinfotech.co.in>
2022-02-18 17:49:23 +0530

    Commit 1.0

--- src/components/HomeTest.js
+++ src/components/HomeTest.js
@@ -7,7 +7,7 @@ const HomeTest = (params) => {
         <>
             <View>
                 <Text>Home Test</Text>
-                <Text>Deploy Android build - 1.0</Text>
+                <Text>Deploy iOS build - 1.0</Text>
             </View>
         </>
     )
